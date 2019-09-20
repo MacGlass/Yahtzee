50a281d81c33dd4a8b6642c675d1cbaf064d45a6
Kathy Nguyen <kathyn262@gmail.com>
2019-09-20 14:34:59 -0700

    finished lab and testing, got code review

--- src/Game.test.js
+++ src/Game.test.js
@@ -3,6 +3,7 @@ import { shallow, mount } from "enzyme";
 // import App from './App';
 import Game from './Game';
 import Die from './Die'
+import RuleRow from './RuleRow'
 
 
 it('renders without crashing', () => {
@@ -14,6 +15,7 @@ it("simulates click and checks that dice has been toggled", function () {
   wrapper.find("button").first()
     .simulate("click");
   expect(wrapper.state().locked).toEqual([true, false, false, false, false])
+  //  check that number didnt change too
 })
 
 it("simulates click when numRolls is zero and checks that dice can't be untoggled", function () {
@@ -27,4 +29,13 @@ it("simulates click when numRolls is zero and checks that dice can't be untoggle
   expect(wrapper.state().rollsLeft).toEqual(0)
 })
 
+it("simulates score in already scored row", function() {
+  let wrapper = mount(<Game />);
+  let ruleRow = wrapper.find('tr.RuleRow-active').first()
+  ruleRow.simulate("click");
 
+  expect(wrapper.state().scores.ones).toEqual(0);
+  let score = wrapper.state().scores.ones;
+  ruleRow.simulate("click");
+  expect(wrapper.state().scores.ones).toEqual(score);
+})
--- src/RuleRow.js
+++ src/RuleRow.js
@@ -3,12 +3,21 @@ import './RuleRow.css'
 
 class RuleRow extends Component {
   render() {
-    return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
-      </tr>
-    )
+    if (this.props.score === undefined) {
+      return (
+        <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
+          <td className="RuleRow-name">{this.props.name}</td>
+          <td className="RuleRow-score">{this.props.score}</td>
+        </tr>
+      )
+    } else {
+      return (
+        <tr className="RuleRow RuleRow-active">
+          <td className="RuleRow-name">{this.props.name}</td>
+          <td className="RuleRow-score">{this.props.score}</td>
+        </tr>
+      )
+    }
   }
 }
 
